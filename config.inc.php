ed6b70365892f585747f26cf1d51b53471c48194
Florian (DF2ET) <florian@florian-wolters.de>
2017-01-31 11:26:11 +0100

    Move config values to separate config file (#6)
    * Move confidential values to config file
    
    * Add config file

---
+++ config.inc.php
@@ -0,0 +1,11 @@
+<?php
+define('BOT_TOKEN', 'insertyourbottokenhere');
+define('API_URL', 'https://api.telegram.org/bot'.BOT_TOKEN.'/');
+define('WEBHOOK_URL', 'insertyourwebhookurlhere');
+
+define('APRSFI_APIKEY', 'insertyouraprsfiapikeyhere');
+
+define('DAPNET_USERNAME', 'insertyourusernamefordapnetportal');
+define('DAPNET_PASSWORD', 'insertyourpasswordfordapnetportal');
+define('DAPNET_URL', 'insertdapnetRESTinterfaceurlhere');
+?>
